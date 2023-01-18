0f6f5d2bbe97ab73f82c16da54f729ed6c2eb183
Ran <huangran.alex@gmail.com>
2023-01-18 14:10:48 +0800

    check changed file
    Signed-off-by: Ran <huangran.alex@gmail.com>

---
+++ scripts/mdxCompiler.js
@@ -0,0 +1,25 @@
+// use mdx-js/mdx to compile file from the input path. If the compilation runs into an error, throw an error
+import fs from "fs/promises";
+import { compile } from "@mdx-js/mdx";
+
+const inputPaths = process.argv.slice(2);
+const errors = [];
+
+const promises = inputPaths.map(async (inputPath) => {
+  try {
+    const input = await fs.readFile(inputPath, "utf8");
+    await compile(input)
+      .then(() => console.log(`${inputPath} was successfully compiled.`))
+      .catch(error => {
+        errors.push(`Error during compilation of ${inputPath} : ${error}`);
+      });
+  } catch (error) {
+    errors.push(`Error during reading file ${inputPath} : ${error}`);
+  }
+});
+
+Promise.all(promises).then(() => {
+  if (errors.length > 0) {
+    throw new Error(errors.join("\n"));
+  }
+});
